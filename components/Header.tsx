961fb5474007caec016074bfef29a55734ca6f9d
Alberto Bautista <dachb101@gmail.com>
2024-03-15 17:04:11 -0600

    Update header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,14 +8,17 @@ import GenreDropdown from "./GenreDropdown";
 const Header = () => {
   return (
     <header className="flex justify-between p-5 bg-gray-300 dark:bg-[#1A1C29] fixed w-full z-50 top-0 items-center ">
-      <Link href="/" className="mr-10">
+      <Link href="/" className="flex items-center justify-center gap-2 mr-10">
         <Image
-          src="https://links.papareact.com/a943ae"
+          src="/movieIcon.svg"
           alt="Disney Logo"
-          width={120}
-          height={100}
+          width={50}
+          height={50}
           className="cursor-pointer invert-0 dark:invert"
         />
+        <h1 className="text-2xl tracking-wider uppercase cursor-pointer dark:text-yellow-500">
+          Movies
+        </h1>
       </Link>
       <div className="flex space-x-2">
         <GenreDropdown />
