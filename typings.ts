19b1f4bb3978d7abd724d7a91e790175442ea121
Alberto Bautista <dachb101@gmail.com>
2024-03-14 20:19:48 -0600

    Add typings file

---
+++ typings.ts
@@ -0,0 +1,38 @@
+export type Company = {
+  id: string;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+};
+export type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path?: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  production_companies: Company[];
+};
+
+export type SearchResults = {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+};
+export type Genre = {
+  id: number;
+  name: string;
+};
+
+export type Genres = {
+  genres: Genre[];
+};
