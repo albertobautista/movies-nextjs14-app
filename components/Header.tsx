300d29b179e114095d437d59034739e15997617f
Alberto Bautista <dachb101@gmail.com>
2024-04-02 17:06:34 -0600

    Update Header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import GenreDropdown from "./GenreDropdown";
 
 const Header = () => {
   return (
-    <header className="flex justify-center flex-wrap sm:justify-between p-5 pl-10 bg-gray-300 dark:bg-[#1A1C29] fixed w-full z-50 top-0 items-center ">
+    <header className="flex justify-between p-5 pl-10 bg-gray-300 dark:bg-[#1A1C29] fixed w-full z-50 top-0 items-center">
       <Link href="/" className="flex items-center justify-center gap-2 mr-10">
         <Image
           src="/movieIcon.svg"
@@ -16,7 +16,7 @@ const Header = () => {
           height={50}
           className="cursor-pointer invert-0 dark:invert"
         />
-        <h1 className="text-2xl tracking-wider uppercase cursor-pointer dark:text-yellow-500">
+        <h1 className="text-2xl hidden sm:block tracking-wider uppercase cursor-pointer dark:text-yellow-500">
           Movies
         </h1>
       </Link>
